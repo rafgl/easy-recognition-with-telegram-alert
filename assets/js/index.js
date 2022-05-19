5fe0d9e0397ee8ada4771ab6fa21603df2274c47
rafgl <rafael.luan.gaedke@hotmail.com>
2022-05-18 21:56:49 -0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,115 @@
+const cam = document.getElementById('cam')
+const startVideo = () => {
+
+    navigator.mediaDevices.enumerateDevices()
+        .then(devices => {
+            // console.log(devices)
+            if (Array.isArray(devices)) {
+                // console.log(devices)
+                devices.forEach(device => {
+                    if (device.kind === 'videoinput') {
+                        console.log(device)
+                        if (device.label.includes('C920')) {
+                            navigator.getUserMedia(
+                                {
+                                    video: {
+                                        deviceId: device.deviceId
+                                    }
+                                },
+                                stream => cam.srcObject = stream,
+                                error => console.error(error)
+                            )
+                        }
+                    }
+                })
+            }
+        })
+}
+
+const sendMessage = () => {
+
+    var apitBot = 'bot5381326320:AAG07R_ARBkUNU9w7kjLjcHTLw7ePSVFtXI';
+    var chatId = '1352459197';
+    var message = 'Sua casa está sendo invada!!!';
+
+    var xhr = new XMLHttpRequest();
+    var url = "https://api.telegram.org/" + apitBot + "/sendmessage?chat_id=" + chatId + "&parse_mode=Markdown&text=" + message + "";
+    xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.send();
+}
+
+const loadLabels = () => {
+    const labels = ['neto']
+    return Promise.all(labels.map(async label => {
+        const descriptions = []
+        for (let i = 1; i <= 5; i++) {
+            const img = await faceapi.fetchImage(`/assets/lib/face-api/labels/${label}/${i}.jpeg`)
+            const detections = await faceapi
+                .detectSingleFace(img)
+                .withFaceLandmarks()
+                .withFaceDescriptor()
+            descriptions.push(detections.descriptor)
+        }
+        return new faceapi.LabeledFaceDescriptors(label, descriptions)
+    }))
+}
+
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/assets/lib/face-api/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/assets/lib/face-api/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/assets/lib/face-api/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/assets/lib/face-api/models'),
+    faceapi.nets.ageGenderNet.loadFromUri('/assets/lib/face-api/models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/assets/lib/face-api/models'),
+]).then(startVideo)
+
+cam.addEventListener('play', async () => {
+    const canvas = faceapi.createCanvasFromMedia(cam)
+    const canvasSize = {
+        width: cam.width,
+        height: cam.height
+    }
+    const labels = await loadLabels()
+    faceapi.matchDimensions(canvas, canvasSize)
+    document.body.appendChild(canvas)
+    setInterval(async () => {
+        const detections = await faceapi
+            .detectAllFaces(
+                cam,
+                new faceapi.TinyFaceDetectorOptions()
+            )
+            .withFaceLandmarks()
+            .withFaceExpressions()
+            .withAgeAndGender()
+            .withFaceDescriptors()
+        const resizedDetections = faceapi.resizeResults(detections, canvasSize)
+        const faceMatcher = new faceapi.FaceMatcher(labels, 0.6)
+        const results = resizedDetections.map(d =>
+            faceMatcher.findBestMatch(d.descriptor)
+        )
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        faceapi.draw.drawDetections(canvas, resizedDetections)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+        resizedDetections.forEach(detection => {
+            const { age, gender, genderProbability } = detection
+            new faceapi.draw.DrawTextField([
+                `${parseInt(age, 10)} years`,
+                `${gender} (${parseInt(genderProbability * 100, 10)})`
+            ], detection.detection.box.topRight).draw(canvas)
+        })
+        results.forEach((result, index) => {
+            const box = resizedDetections[index].detection.box
+            const message = sendMessage()
+
+            const { label, distance } = result
+
+            label == 'unknown' ? message : null;
+
+            new faceapi.draw.DrawTextField([
+                `${label != 'unknown' ? label : 'Pessoa desconhecida'}   (${parseInt(distance * 100, 10)})`
+            ], box.bottomRight).draw(canvas)
+        })
+    }, 100)
+})
